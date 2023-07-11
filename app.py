bb4827a74f58e072e42534b435c4b0c8d20179d3
bzmay0149 <mayjuanbaticono@gmail.com>
2023-07-11 12:10:56 +0200

    inicio back

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from dataBase.connect_db import create_connection
+def create_app():
+    app=Flask(__name__) 
+    return app
+
+app = create_app()
+
+create_connection()
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ readme.txt
@@ -0,0 +1,10 @@
+##create virtual environment
+python3 -m venv .venv
+
+
+##Activate virtual environment
+. .venv/bin/activate
+
+
+##nstall libraries
+pip install -r requirements.txt
