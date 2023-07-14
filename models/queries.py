1bb7683dfb5f7ae5826ac32e465d68d4eb86659b
bzmay0149 <mayjuanbaticono@gmail.com>
2023-07-14 14:25:46 +0200

    se agrego cliente

--- models/queries.py
+++ models/queries.py
@@ -1,12 +1,6 @@
-from flask import Flask, request
 from database.connect_db import create_connection
-from models.queries import add_cliente
-from routes.post import *
+from flask import request
 
-app = Flask(__name__)
-
-
-@app.route('/', methods=['POST'])
 def add_cliente():
     try:
         con = create_connection()
@@ -21,14 +15,15 @@ def add_cliente():
         address = postman[ 'address']
         city = postman['city']
         province = postman['province']
-        postal_code = postman['postalcode']
+        postalcode = postman['postalcode']
         budget = postman['budget']
         accepted_budget = postman['accepted_budget']
+        term = postman['term']
         done = postman['done']
         invoiced = postman['invoiced']
         date = postman['date']
         
-        cur.execute("INSERT INTO customers (name, lastname, phone, email, address, city, province, postal_code, budget, accepted_budget,  done, invoiced, date) VALUES (%s,  %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (name, lastname, phone, email, address, city, province, postal_code, budget, accepted_budget, done, invoiced, date))
+        cur.execute("INSERT INTO client (name, lastname, phone, email, address, city, province, postalcode, budget, accepted_budget, term, done, invoiced, date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (name, lastname, phone, email, address, city, province, postalcode, budget, accepted_budget, term, done, invoiced, date))
         con.commit()
         con.close()
         return {'message': 'Client added successfully'}, 201
@@ -37,9 +32,11 @@ def add_cliente():
         print(e)
         con.close()
         return {'message': 'Error adding client'}, 500
-        print(e)
-    pass
+  
 
-   
-if __name__ == '__main__':
-    app.run(debug=True)
+        
+    
+
+
+
+    
